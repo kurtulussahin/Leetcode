522a1d0fdce8d2fd29df704335c75a98a785dc70
Kurtuluş Şahin <kurtulussahin@gmail.com>
2024-10-20 16:05:57 +0300

    Time: 28 ms (18.6%), Space: 45.2 MB (26.77%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -7,27 +7,20 @@ class Solution {
             right = Math.max(right, pile);
         }
 
-        while (left<right){
-            int mid = left + (right-left)/2;
-            if(canEat(piles, mid, h)){
-                right=mid;
-            }else{
-                left=mid+1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int hourSpent = 0;
+            for (int pile : piles) {
+                hourSpent += Math.ceil((double) pile / mid);
             }
+
+            if (hourSpent <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+
         }
-
-        return left;
-
-
-    }
-
-    public boolean canEat(int piles[], int k, int h){
-        int hours = 0;
-        for(int pile : piles){
-            int div = pile / k;
-            hours += div;
-            if(pile % k != 0) hours++;
-        }
-        return hours <= h;
+        return right;
     }
 }
