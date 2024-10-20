988fc49f4d2aaf8e67afde7824751f4845d8ceea
Kurtuluş Şahin <kurtulussahin@gmail.com>
2024-10-20 18:59:49 +0300

    Time: 2 ms (98.77%), Space: 45.1 MB (19.01%) - LeetHub

--- 0001-two-sum/0001-two-sum-onePassHashMap.java
+++ 0001-two-sum/0001-two-sum-onePassHashMap.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int result[]=new int[2];
-        Map<Integer, Integer> complementMap = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
 
-        for(int i=0; i<nums.length; i++){
-            complementMap.put(nums[i], i);
-        }
-
-        for(int i=0; i<nums.length; i++){
-            int complement=target-nums[i];
-            if(complementMap.containsKey(complement) && complementMap.get(complement) !=i){
-                result[0]=i;
-                result[1]=complementMap.get(complement);
+        for(int i= 0; i<nums.length; i++){
+            int complement =target-nums[i];
+            if(map.containsKey(complement) && i!=map.get(complement)){
+                result[0]=map.get(target-nums[i]);
+                result[1]=i;
                 return result;
+
             }
+            map.put(nums[i], i);
         }
 
         return result;
-
+        
     }
 }
