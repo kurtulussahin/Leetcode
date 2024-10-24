50b329054eb57c512428e8632b74c3a64405ac91
Kurtuluş Şahin <kurtulussahin@gmail.com>
2024-10-24 21:28:31 +0300

    Update 0015-3sum-bruteForce.java

--- 0015-3sum/0015-3sum-bruteForce.java
+++ 0015-3sum/0015-3sum-bruteForce.java
@@ -1,40 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+
+        Set<List<Integer>> result = new HashSet<>();
         Arrays.sort(nums);
 
-        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    if(nums[i]+nums[j]+nums[k]==0){
+                        List<Integer> tmp=new ArrayList<>();
+                        tmp.add(nums[i]);
+                        tmp.add(nums[j]);
+                        tmp.add(nums[k]);
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                break;
-            }
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum > 0) {
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        left++;
+                        result.add(tmp);
+                        
                     }
                 }
             }
-
         }
 
-        return result;
 
+        return new ArrayList <>(result);
+        
     }
 }
